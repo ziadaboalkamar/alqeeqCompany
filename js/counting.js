fff49d030e58daa2f1e1bb3df5db38f97d776841
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-05-24 13:37:41 +0300

    ziad

--- js/counting.js
+++ js/counting.js
@@ -1,3 +1,23 @@
+var counterTeaserL = $('.number_of_client');
+var winHeight = $(window).height();
+if (counterTeaserL.length) {
+    var firEvent = false,
+        objectPosTop = $('.number_of_client').offset().top;
+        
+        //when element shows at bottom
+        var elementViewInBottom = objectPosTop - winHeight;
+    $(window).on('scroll', function() {
+        var currentPosition = $(document).scrollTop();
+        //when element position starting in viewport
+      if (currentPosition > elementViewInBottom && firEvent === false) {
+        firEvent = true;
+        animationCounter();
+      }   
+    });
+}
+
+//counter function will animate by using external js also add seprator "."
+function animationCounter(){
 $('.counting').each(function() {
     var $this = $(this),
         countTo = $this.attr('data-count');
@@ -9,16 +29,17 @@ $('.counting').each(function() {
     {
   
       duration: 3000,
-      easing:'linear',
+      easing: 'swing',
       step: function() {
         $this.text(Math.floor(this.countNum));
       },
       complete: function() {
         $this.text(this.countNum);
-        //alert('finished');
+        // alert('finished');
       }
   
     });  
     
   
   });
+}
